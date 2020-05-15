907595cfcccac3148e29778cdce0991b9ff2252e
Stéphane Brunner <stephane.brunner@camptocamp.com>
2020-05-15 16:13:17 +0200

    Black formatting

--- es_oom_exporter/es.py
+++ es_oom_exporter/es.py
@@ -15,19 +15,21 @@ from es_oom_exporter.utils import ensure_slash
 # Sep 19 08:35:40 ip-10-10-10-56 kernel: Memory cgroup out of memory: Kill process 99190 (apache2) score 1534 or sacrifice child  # noqa: E501
 
 LOG = logging.getLogger(__name__)
-START_RE = re.compile(r".* ([^ ]+) kernel: Task in /kubepods\.slice/kubepods-burstable\.slice/"
-                      r"kubepods-burstable-pod([0-9a-f_]*)\.slice/docker-([0-9a-f]*)\.scope killed as a result of "
-                      r"limit of /kubepods.slice/kubepods-burstable.slice/kubepods-burstable-pod[0-9a-f_]*\.slice")
-CONTAINER_RE = re.compile(r".* ([^ ]+) kernel: Memory cgroup stats for /kubepods\.slice/kubepods-burstable\.slice/"
-                          r"kubepods-burstable-pod([0-9a-f_]*)\.slice/docker-([0-9a-f]*)\.scope:.* rss:(\d+[KMG]B).*")
-OOM_RE = re.compile(r".* ([^ ]+) kernel: Memory cgroup out of memory: Kill process \d+ \(([^)]+)\) "
-                    r"score \d+ or sacrifice child")
+START_RE = re.compile(
+    r".* ([^ ]+) kernel: Task in /kubepods\.slice/kubepods-burstable\.slice/"
+    r"kubepods-burstable-pod([0-9a-f_]*)\.slice/docker-([0-9a-f]*)\.scope killed as a result of "
+    r"limit of /kubepods.slice/kubepods-burstable.slice/kubepods-burstable-pod[0-9a-f_]*\.slice"
+)
+CONTAINER_RE = re.compile(
+    r".* ([^ ]+) kernel: Memory cgroup stats for /kubepods\.slice/kubepods-burstable\.slice/"
+    r"kubepods-burstable-pod([0-9a-f_]*)\.slice/docker-([0-9a-f]*)\.scope:.* rss:(\d+[KMG]B).*"
+)
+OOM_RE = re.compile(
+    r".* ([^ ]+) kernel: Memory cgroup out of memory: Kill process \d+ \(([^)]+)\) "
+    r"score \d+ or sacrifice child"
+)
 SIZE_RE = re.compile(r"^(\d+)([KMG])B$")
-SIZES = {
-    'K': 1024,
-    'M': 1024 * 1024,
-    'G': 1024 * 1024 * 1024
-}
+SIZES = {"K": 1024, "M": 1024 * 1024, "G": 1024 * 1024 * 1024}
 
 
 class Oom:
@@ -53,11 +55,11 @@ class Oom:
         self._container_uid = matcher.group(3)
         pod_info = pod_infos.get(pod_uid)
         if pod_info is not None:
-            self._pod_name = pod_info['pod_name']
-            self._namespace = pod_info['namespace']
-            self._release = pod_info['release']
-            self._service = pod_info['service']
-            container_info = pod_info['containers'].get(self._container_uid)
+            self._pod_name = pod_info["pod_name"]
+            self._namespace = pod_info["namespace"]
+            self._release = pod_info["release"]
+            self._service = pod_info["service"]
+            container_info = pod_info["containers"].get(self._container_uid)
             if container_info is not None:
                 self._container = container_info
             else:
@@ -119,16 +121,16 @@ class Oom:
 
 class ElasticSearch:
     def __init__(self):
-        es_url = ensure_slash(os.environ['ES_URL'])
-        es_indexes = os.environ.get('ES_INDEXES', '_all')
-        es_auth = os.environ.get('ES_AUTH')
+        es_url = ensure_slash(os.environ["ES_URL"])
+        es_indexes = os.environ.get("ES_INDEXES", "_all")
+        es_auth = os.environ.get("ES_AUTH")
         self.search_headers = {
             "Content-Type": "application/json;charset=UTF-8",
             "Accept": "application/json",
-            "kbn-version": "6.8.0"
+            "kbn-version": "6.8.0",
         }
         if es_auth is not None:
-            self.search_headers['Authorization'] = es_auth
+            self.search_headers["Authorization"] = es_auth
         self.search_url = f"{es_url}{es_indexes}/_search"
         self.last_timestamp = int(time.time() * 1000)
 
@@ -136,88 +138,49 @@ class ElasticSearch:
         query = {
             "version": True,
             "size": 500,
-            "sort": [
-                {
-                    "log.offset": {
-                        "order": "asc",
-                        "unmapped_type": "boolean"
-                    }
-                }
-            ],
-            "docvalue_fields": [
-                {
-                    "field": "@timestamp",
-                    "format": "epoch_millis"
-                }
-            ],
+            "sort": [{"log.offset": {"order": "asc", "unmapped_type": "boolean"}}],
+            "docvalue_fields": [{"field": "@timestamp", "format": "epoch_millis"}],
             "query": {
                 "bool": {
-                    "must": [
-                        {
-                            "match_all": {}
-                        }
-                    ],
+                    "must": [{"match_all": {}}],
                     "filter": [
+                        {"match_phrase": {"log.file.path": {"query": "/var/log/messages"}}},
+                        {"match_phrase": {"message": {"query": "kernel"}}},
                         {
-                            "match_phrase": {
-                                "log.file.path": {
-                                    "query": "/var/log/messages"
-                                }
-                            }
-                        }, {
-                            "match_phrase": {
-                                "message": {
-                                    "query": "kernel"
-                                }
-                            }
-                        }, {
                             "bool": {
                                 "should": [
-                                    {
-                                        "match_phrase": {
-                                            "message": "Memory cgroup stats for"
-                                        }
-                                    }, {
-                                        "match_phrase": {
-                                            "message": "Memory cgroup out of memory"
-                                        }
-                                    }, {
-                                        "match_phrase": {
-                                            "message": "killed as a result of limit of"
-                                        }
-                                    }
+                                    {"match_phrase": {"message": "Memory cgroup stats for"}},
+                                    {"match_phrase": {"message": "Memory cgroup out of memory"}},
+                                    {"match_phrase": {"message": "killed as a result of limit of"}},
                                 ],
-                                "minimum_should_match": 1
+                                "minimum_should_match": 1,
                             }
-                        }
-                    ]
+                        },
+                    ],
                 }
-            }
+            },
         }
         if self.last_timestamp is not None:
-            query['query']['bool']['filter'].append({  # type: ignore
-                "range": {
-                    "@timestamp": {
-                        "gt": self.last_timestamp,
-                        "format": "epoch_millis"
-                    }
+            query["query"]["bool"]["filter"].append(
+                {  # type: ignore
+                    "range": {"@timestamp": {"gt": self.last_timestamp, "format": "epoch_millis"}}
                 }
-            })
+            )
         with requests.post(self.search_url, json=query, headers=self.search_headers) as r:
             if r.status_code != 200:
                 LOG.warning("Error from ES: %s", r.text)
             r.raise_for_status()
             json = r.json()
-            hits = json['hits']['hits']
+            hits = json["hits"]["hits"]
             if len(hits) == 0:
                 return []
             pod_infos = kube.get_pod_infos()
             cur_by_host: Dict[str, Oom] = {}
             ooms = []
             for hit in hits:
-                timestamp = int(hit['fields']['@timestamp'][0])
+                timestamp = int(hit["fields"]["@timestamp"][0])
                 self.last_timestamp = timestamp
-                message = hit['_source']['message']
+                message = hit["_source"]["message"]
                 LOG.debug("message: %s", message)
                 start_match = START_RE.match(message)
                 pod_match = CONTAINER_RE.match(message)
